36d4cc7836b5d3f5f845b1bdad980e63c3a41ffd
Elianis Manuel Medina Moron <elianismedina05@outlook.com>
2023-11-30 09:40:21 -0500

    Update project page layout

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,18 +4,10 @@ import Image from "next/image";
 export default function ProjectPage() {
   return (
     <>
-      <div className="mx-auto mb-12 mr-4 mt-0 grid grid-cols-1 gap-16 bg-slate-50 p-8 py-2 md:grid-cols-2">
+      <div className="mx-auto mb-12 mr-4 mt-24 grid grid-cols-1 gap-16 bg-slate-50 p-8 py-2 md:grid-cols-2">
+        <div></div>
         <div>
-          <Image
-            src="/public/images/Vector.svg"
-            alt="Logo"
-            width={100}
-            height={100}
-            className="rounded-lg"
-          />
-        </div>
-        <div>
-          <h1 className="text-4xl">Proyectos</h1>
+          <h1 className="mt-4 text-4xl">Proyectos</h1>
         </div>
         <div className="relative mx-auto gap-2">
           <div className="absolute -right-8 top-12 h-32 w-2/3 rounded-lg bg-sky-800 text-2xl text-white opacity-50">
