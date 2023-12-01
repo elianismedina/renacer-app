11727beff365d7808243b5ffa916d45d8fd2aba2
Elianis Manuel Medina Moron <elianismedina05@outlook.com>
2023-12-01 15:57:43 -0500

    worked on news page

---
+++ src/app/noticias/components/dialogbox.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import ShareIcon from "@mui/icons-material/Share";
+import Slide from "@mui/material/Slide";
+import { type TransitionProps } from "@mui/material/transitions";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function DialogBox() {
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        <MoreVertIcon />
+      </Button>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogActions>
+          <Button onClick={handleClose}>
+            <ShareIcon />
+            <p className="p-2">Compartir entrada</p>
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
--- src/app/noticias/page.tsx
+++ src/app/noticias/page.tsx
@@ -1,11 +1,14 @@
-export const metadata = {
-  title: "noticias | RENACER",
-  description: "Consejo comunitario",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
-};
+"use client";
+import NewsCard from "./components/newscard";
 
 const NoticiasPage = () => {
-  return <div className="mt-24">Noticias Page</div>;
+  return (
+    <div className="mx-auto mb-8 mt-28 grid w-3/4 grid-cols-1 gap-2 md:grid-cols-3">
+      <NewsCard />
+      <NewsCard />
+      <NewsCard />
+    </div>
+  );
 };
 
 export default NoticiasPage;
